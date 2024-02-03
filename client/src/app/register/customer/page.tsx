9bb7bfa73c4a14c761a14217185c06a0cf28a294
Cleyton Alves <cleyton.alves.a@gmail.com>
2024-02-03 04:51:20 -0300

    feat: :sparkles: Page sign up created

---
+++ client/src/app/register/customer/page.tsx
@@ -0,0 +1,26 @@
+import Register from '@/app/components/register'
+import Image from 'next/image'
+import React from 'react'
+
+export default function RegisterCustomer() {
+  return (
+    <>
+      <div className='flex mx-auto bg-gray-100'>
+        <div className='flex  w-screen'>
+        <div className='flex mx-auto justify-center items-center w-full'>
+            <Register />
+        </div>
+        <div className='flex h-screen w-full'>
+          <Image
+            src="/Login7Support.jpg"
+            width={500}
+            height={500}
+            alt='Image home page'
+            className=' flex mx-auto h-full w-full'
+          />
+      </div>
+    </div>
+      </div>
+    </>
+  )
+}
