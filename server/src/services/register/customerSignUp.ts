9038d7ab77f216555ba504a7e1899f0c757d3239
Cleyton Alves <cleyton.alves.a@gmail.com>
2024-02-22 10:52:53 -0300

    refactor: :lock: The Signup has been separated into categories customer, manager ,and mechanic.

--- server/src/services/register/customerSignUp.ts
+++ server/src/services/register/customerSignUp.ts
@@ -1,9 +1,25 @@
 import { prisma } from '../../db/prismaClient';
+import { cpfValidate } from '../validations/cpf/cpfValidate';
+import { existingCPFCustomer } from '../validations/cpf/existingCPFCustomer';
+import { existingCPFManager } from '../validations/cpf/existingCPFManager';
+import { existingCPFMechanic } from '../validations/cpf/existingCPFMechanic';
+import { emailEmpty } from '../validations/email/emailEmpty';
+import { emailFormat } from '../validations/email/emailFormat';
+import { passwordFormat } from '../validations/password/passwordFormat';
 import { hashPassword } from './passwordService';
 
 
 export const customerSignUp = async ( email:string, password:string, name:string, cpf:string, role:string ) => {
-  const hashedPassword = await hashPassword(password);    
+  const hashedPassword = await hashPassword(password);
+  await Promise.all([
+      emailEmpty(email),
+      emailFormat(email),
+      passwordFormat(password),
+      cpfValidate(cpf),
+      existingCPFCustomer(cpf),
+      existingCPFManager(cpf),
+      existingCPFMechanic(cpf),
+  ]);
   await prisma.customer.create({
       data: {
         name, 
--- server/src/services/register/managerSignUp.ts
+++ server/src/services/register/managerSignUp.ts
@@ -2,10 +2,26 @@ import httpStatus from 'http-status';
 import { prisma } from '../../db/prismaClient';
 import HttpException from '../../middleware/errors/HttpException';
 import { hashPassword } from './passwordService';
+import { emailEmpty } from '../validations/email/emailEmpty';
+import { emailFormat } from '../validations/email/emailFormat';
+import { passwordFormat } from '../validations/password/passwordFormat';
+import { cpfValidate } from '../validations/cpf/cpfValidate';
+import { existingCPFCustomer } from '../validations/cpf/existingCPFCustomer';
+import { existingCPFManager } from '../validations/cpf/existingCPFManager';
+import { existingCPFMechanic } from '../validations/cpf/existingCPFMechanic';
 
 
 export const managerSignUp = async ( email:string, password:string, name:string, cpf:string, role:string ) => {
   const hashedPassword = await hashPassword(password);
+  await Promise.all([
+      emailEmpty(email),
+      emailFormat(email),
+      passwordFormat(password),
+      cpfValidate(cpf),
+      existingCPFCustomer(cpf),
+      existingCPFManager(cpf),
+      existingCPFMechanic(cpf),
+  ]);
   try {
     await prisma.manager.create({
       data: {
