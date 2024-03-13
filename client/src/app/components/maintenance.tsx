062f9731bf18519e1ac81b7e883c33add952a26a
Cleyton Alves <cleyton.alves.a@gmail.com>
2024-03-13 15:40:10 -0300

    style: :lipstick: Modification to the style of the service form.

--- client/src/app/components/maintenance.tsx
+++ client/src/app/components/maintenance.tsx
@@ -11,7 +11,7 @@ export default function MaintenanceForm() {
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     setFormData({
       ...formData, [e.target.id]: e.target.value
     })
@@ -57,41 +57,65 @@ export default function MaintenanceForm() {
   }, [message]);
 
   return (
+    <>
     <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-      <h1 className='flex justify-center items-center font-bold text-gray-400 text-2xl mt-6 mb-8'>Cadastrar Serviço</h1>
-      <div className="relative z-0 w-full mb-5 group">
-          <input 
-            type="text" 
-            name="repair" 
-            id="repair"
-            value={formData.repair || ''}
-            onChange={handleChange}
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required />
-        <label
-          htmlFor="repair"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >Serviço a realizar
-        </label>
+      <h1 
+      className='flex justify-center items-center font-bold 
+    text-gray-400 text-2xl mt-6 mb-8'
+      >Cadastrar Serviço
+      </h1>
+      <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+          <div className="w-full">
+              <label 
+              htmlFor="price" 
+              className="block mb-2 text-sm font-bold text-gray-500"
+              >Valor do Serviço
+              </label>
+              <input 
+              type="text" 
+              name="value" 
+              id="value"
+              value={formData.value || ''}
+              onChange={handleChange} 
+              className="bg-gray-50 border border-gray-500 text-gray-900 
+              text-sm rounded-lg focus:ring-1 focus:outline-none focus:ring-blue-500 
+              block w-full p-2.5"
+              placeholder="R$15,50" />
+          </div>
+          <div className="sm:col-span-2">
+              <label 
+              htmlFor="description" 
+              className="block mb-2 text-sm font-bold text-gray-500 "
+              >Descrição do reparo
+              </label>
+              <textarea 
+              name="repair" 
+              id="repair"
+              value={formData.repair || ''}
+              onChange={handleChange} 
+              rows={3} 
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 
+              rounded-lg border border-gray-500 
+              focus:ring-1 focus:outline-none focus:ring-blue-500"
+              placeholder="Descreva o serviço que será realizado aqui!"/>
+          </div>
+          <button 
+          disabled={loading} 
+          type="submit" 
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+          focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+          text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >{loading ? 'Loading...' : 'Cadastrar'}
+        </button>
       </div>
-      <div className="relative z-0 w-full mb-5 group">
-          <input 
-            type="text" 
-            name="value" 
-            id="value"
-            value={formData.value || ''}
-            onChange={handleChange}
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-          <label htmlFor="value" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Valor unitário R$</label>
-      </div>
-      <button disabled={loading} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{loading ? 'Loading...' : 'Cadastrar'}</button>
-      <div className='mb-8 h-12'>
-        {!success ? (
-          <p className='flex justify-center mt-4 text-red-600'>{ message }</p>
-        ) : (
-          <p className='flex justify-center mt-4 text-teal-600'>{message}</p>
-        )}
-      </div>
-    </form>
-
+        <div className='mb-8 h-12 mt-10 text-xl'>
+          {!success ? (
+            <p className='flex justify-center mt-4 text-red-600'>{ message }</p>
+          ) : (
+            <p className='flex justify-center mt-4 text-teal-600'>{message}</p>
+          )}
+        </div>
+      </form>
+    </>
   )
 }
--- client/src/app/components/product.tsx
+++ client/src/app/components/product.tsx
@@ -130,6 +130,5 @@ export default function ProductForm() {
           )}
         </div>
     </form>
-
   )
 }
