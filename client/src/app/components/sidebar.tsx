1052c7a4178e25060a2bfa7ee28d97edcbbb82e2
Cleyton Alves <cleyton.alves.a@gmail.com>
2024-03-18 11:15:10 -0300

    feat: :sparkles: Included search for products and maintenance in the search button on sidebar

--- client/src/app/components/sidebar.tsx
+++ client/src/app/components/sidebar.tsx
@@ -4,47 +4,52 @@ import Link from 'next/link';
 import { ChevronsDown, PencilLine, Barcode, Settings, Package, SlidersHorizontal, Menu, Search } from 'lucide-react';
 
 export default function Sidebar() {
-  const [isCadastrarOpen, setIsCadastrarOpen] = useState(false);
-  const [isProdutoServicoOpen, setIsProdutoServicoOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isRegisterOpen, setIsCadastrarOpen] = useState(false);
+   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
-  const toggleCadastrar = () => {
-    setIsCadastrarOpen(!isCadastrarOpen);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
-  const toggleProdutoServico = () => {
-    setIsProdutoServicoOpen(!isProdutoServicoOpen);
+  const toggleRegister = () => {
+    setIsCadastrarOpen(!isRegisterOpen);
+  };
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
   };
 
   return (
-    <aside className={`flex flex-col items-center justify-start w-16 h-screen bg-light-blue text-white overflow-hidden transition-all ${isCadastrarOpen ? 'w-64 transition-bg duration-500' : 'w-16 transition-bg duration-500'}`}>
+    <aside className={`flex flex-col items-center justify-start w-16 h-screen bg-light-blue text-white overflow-hidden transition-all ${isMenuOpen ? 'w-64 transition-bg duration-500' : 'w-16 transition-bg duration-500'}`}>
       <div className="flex items-center mb-4">
         <button
           className="flex text-white hover:text-gray-100 focus:outline-none items-end"
-          onClick={toggleCadastrar}
+          onClick={toggleMenu}
         >
           <Menu />
-          {isCadastrarOpen ? <p className='transition-opacity ml-4 hover:text-gray-100'>Menu</p> : ''}
+          {isMenuOpen ? <p className='transition-opacity ml-4 hover:text-gray-100'>Menu</p> : ''}
           
         </button>
       </div>
 
-      <div className={`transition-opacity mb-2 w-full ${isCadastrarOpen ? 'opacity-100 transition-bg duration-500' : 'opacity-0 transition-bg duration-500'}`}>
+      <div className={`transition-opacity mb-2 w-full ${isMenuOpen ? 'opacity-100 transition-bg duration-500' : 'opacity-0 transition-bg duration-500'}`}>
         <button
           className="flex items-center justify-between w-full px-6 py-2 text-gray-200 hover:text-gray-400 focus:outline-none hover:bg-white transition-bg duration-500"
-          onClick={toggleProdutoServico}
+          onClick={toggleRegister}
         >
           <div className="flex items-center">
             <PencilLine className="w-6 h-6"/>
             <span className='ml-4'>Cadastrar</span>
           </div>
           <p
-            className={`h-5 w-5 ml-4 ${isProdutoServicoOpen ? 'transform rotate-90 transition-bg duration-500' : 'transition-bg duration-500'}`}
+            className={`h-5 w-5 ml-4 ${isRegisterOpen ? 'transform rotate-90 transition-bg duration-500' : 'transition-bg duration-500'}`}
           >
             <ChevronsDown />
           </p>
         </button>
 
-        {isProdutoServicoOpen && (
+        {isRegisterOpen && (
           <div>
             <div className="flex items-center justify-between w-full px-6 py-2 text-gray-200 hover:text-gray-400 focus:outline-none transition-bg duration-500 hover:bg-white">
               <Link href="/admin/register/product" className='flex items-center gap-2 ml-8 hover:ml-6'>
@@ -61,23 +66,23 @@ export default function Sidebar() {
           </div>
         )}
       </div>
-      <div className={`transition-opacity mb-2 w-full ${isCadastrarOpen ? 'opacity-100 transition-bg duration-500' : 'opacity-0 transition-bg duration-500'}`}>
+      <div className={`transition-opacity mb-2 w-full ${isMenuOpen ? 'opacity-100 transition-bg duration-500' : 'opacity-0 transition-bg duration-500'}`}>
         <button
           className="flex items-center justify-between w-full px-6 py-2 text-gray-200 hover:text-gray-400 focus:outline-none hover:bg-white transition-bg duration-500"
-          onClick={toggleProdutoServico}
+          onClick={toggleSearch}
         >
           <div className="flex items-center">
             <Search className="w-6 h-6"/>
             <span className='ml-4'>Pesquisar</span>
           </div>
           <p
-            className={`h-5 w-5 ml-4 ${isProdutoServicoOpen ? 'transform rotate-90 transition-bg duration-500' : 'transition-bg duration-500'}`}
+            className={`h-5 w-5 ml-4 ${isSearchOpen ? 'transform rotate-90 transition-bg duration-500' : 'transition-bg duration-500'}`}
           >
             <ChevronsDown />
           </p>
         </button>
 
-        {isProdutoServicoOpen && (
+        {isSearchOpen && (
           <div>
             <div className="flex items-center justify-between w-full px-6 py-2 text-gray-200 hover:text-gray-400 focus:outline-none transition-bg duration-500 hover:bg-white">
               <Link href="/admin/search/product" className='flex items-center gap-2 ml-8 hover:ml-6'>
@@ -95,7 +100,7 @@ export default function Sidebar() {
         )}
       </div>
 
-      <div className={`transition-opacity w-full flex flex-col items-center justify-start ${isCadastrarOpen ? 'opacity-100' : 'opacity-0'}`}>
+      <div className={`transition-opacity w-full flex flex-col items-center justify-start ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`}>
         <nav className='h-full w-full'>
           <div className="flex items-center justify-between w-full px-6 py-2 text-gray-200 hover:text-gray-400 focus:outline-none transition-bg duration-300 hover:bg-white">
             <Link href="/orders" className='flex space-x-4'>
